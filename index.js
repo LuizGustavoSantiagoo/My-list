5804168811779db656325bed26adf9ffa806a478
LuizGustavoSantiago <128108006+LuizGustavoSantiagoo@users.noreply.github.com>
2024-06-18 08:21:58 -0300

    .

--- index.js
+++ index.js
@@ -7,27 +7,22 @@ async function produtos() {
 
     fetch(url)
         .then((res) => res.json())
-        .then((json) => {
-            array = json;
-            console.log(json)
-            return array;
-        })
         .then((array) => array.forEach(element => {
-            console.log(array)
-        }))
-        .then((array) => {
-                impresao += `
-                <div class="card" style="width: 30%;">
-                    <img class="card-img-top" src="" alt="Card image cap">
-                    <div class="card-body">
-                        <h5 class="card-title"></h5>
-                        <p></p>
-                        <a href="#" class="btn btn-primary"><i class="bi bi-heart"></i></a>
-                    </div>
+            console.log(element)
+            
+            impresao += `
+            <div class="card">
+                <img class="card-img-top" src="${element.image}" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">${element.title}</h5>
+                    <p>${element.description}</p>
+                    <a href="#" class="btn btn-primary"><i class="bi bi-heart"></i></a>
                 </div>
-                `,
-                place.innerHTML = impresao}
-        );
+            </div>
+            `
+            place.innerHTML = impresao;
+        }))
 }
+
 
 produtos();
